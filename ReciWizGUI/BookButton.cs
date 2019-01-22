d33ee7fa7488a358e5f9a82f797c2203cc01bc7e
Molex <simplesleeptimer@gmail.com>
2019-01-22 14:23:49 +0100

    Remove unused field from bookbutton

--- ReciWizGUI/BookButton.cs
+++ ReciWizGUI/BookButton.cs
@@ -11,7 +11,6 @@ namespace ReciWizGUI
     public partial class BookButton : Button
     {
         public int Id;
-        private Action clickAction;
 
         public BookButton(int id, string name, RoutedEventHandler handler) : base()
         {
