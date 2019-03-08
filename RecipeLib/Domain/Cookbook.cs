d4369c0e02c0d4de45d8c74bfb444e9edf1c65b4
Molex <simplesleeptimer@gmail.com>
2019-03-08 20:04:01 +0100

    pretty up CookBook

--- RecipeLib/Domain/Cookbook.cs
+++ RecipeLib/Domain/Cookbook.cs
@@ -17,21 +17,19 @@ namespace RecipeLib.Domain
 
         private IDB db = new LiteConnector();
 
-        private IngredientRepo ingredientRepo = new IngredientRepo();
-
-        private readonly List<IRecipe> Recipies = new List<IRecipe>();
+        private readonly List<IRecipe> _recipes;
 
         public Cookbook(string name, string author, int id)
         {
             Title = name;
             Author = author;
             ID = id;
-            Recipies = db.GetRecipies(id);
+            _recipes = db.GetRecipies(id);
         }
 
         public Recipe GetRecipe(int id)
         {
-            foreach (Recipe recipe in Recipies) {
+            foreach (Recipe recipe in _recipes) {
                 if (id == recipe.ID) {
                     return recipe;
                 }
@@ -42,7 +40,7 @@ namespace RecipeLib.Domain
         public List<IRecipe> GetRecipes()
         {
             List<IRecipe> recipies = new List<IRecipe>();
-            foreach(IRecipe recipe in Recipies) {
+            foreach(IRecipe recipe in _recipes) {
                 recipies.Add(recipe);
             }
             return recipies;
@@ -53,19 +51,19 @@ namespace RecipeLib.Domain
 
             int recipeID = db.CreateRecipe(ID, name, ingredientsData, instructions);
             Recipe recipe = new Recipe(name, instructions, ingredientsData, recipeID);
-            Recipies.Add(recipe);
+            _recipes.Add(recipe);
         }
 
         private void AddRecipe(Recipe recipe)
         {
-            Recipies.Add(recipe);
+            _recipes.Add(recipe);
         }
 
         public void DeleteRecipe(int recipeID)
         {
             Recipe reci = GetRecipe(recipeID);
             db.DeleteRecipe(recipeID);
-            Recipies.Remove(reci);
+            _recipes.Remove(reci);
 
         }
     }
